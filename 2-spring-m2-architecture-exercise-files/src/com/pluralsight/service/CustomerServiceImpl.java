5ba6e187c7f0cf3fce606aba8ac9be4f29b29f88
lvypas <lyubic@gmail.com>
2013-11-18 18:12:35 +0200

    Changed to constructor based injection.

--- 2-spring-m2-architecture-exercise-files/src/com/pluralsight/service/CustomerServiceImpl.java
+++ 2-spring-m2-architecture-exercise-files/src/com/pluralsight/service/CustomerServiceImpl.java
@@ -9,6 +9,14 @@ public class CustomerServiceImpl implements CustomerService {
 
 	private CustomerRepository customerRepository;
 	
+	public CustomerServiceImpl() {
+		
+	}
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
 	public void setCustomerRepository(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
