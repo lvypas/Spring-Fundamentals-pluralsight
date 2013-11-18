0b24e9236b145d9fce8985221379d2c03ff495fa
lvypas <lyubic@gmail.com>
2013-11-18 18:06:25 +0200

    Added applicationContext.xml to application. Setter based injection.

--- 2-spring-m2-architecture-exercise-files/src/Application.java
+++ 2-spring-m2-architecture-exercise-files/src/Application.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
 
@@ -8,7 +11,9 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		CustomerService service = new CustomerServiceImpl();
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
 		
 		System.out.println(service.findAll().get(0).getFirstname());
 	}
