089b3313301054c99193cf32c19fc2565dbdfb65
lvypas <lyubic@gmail.com>
2013-11-19 19:10:06 +0200

    Added property file. Used java configuration.

--- 2-spring-m2-architecture-exercise-files/src/AppConfig.java
+++ 2-spring-m2-architecture-exercise-files/src/AppConfig.java
@@ -1,14 +1,22 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan("com.pluralsight")
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPSourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	@Bean(name = "customerService")
 	@Scope("prototype")
