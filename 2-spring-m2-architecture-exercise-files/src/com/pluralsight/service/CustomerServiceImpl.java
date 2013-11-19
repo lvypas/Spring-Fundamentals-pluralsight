91eda8ad46960f5c9ca1f4e51049caad9eb607fb
lvypas <lyubic@gmail.com>
2013-11-19 13:42:23 +0200

    Added java based injection.

--- 2-spring-m2-architecture-exercise-files/src/com/pluralsight/service/CustomerServiceImpl.java
+++ 2-spring-m2-architecture-exercise-files/src/com/pluralsight/service/CustomerServiceImpl.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
 
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	private CustomerRepository customerRepository;
 	
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
 	}
